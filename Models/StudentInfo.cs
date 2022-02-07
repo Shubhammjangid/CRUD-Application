a651dcf06315b8dff87a2039d5393090fd5ac82e
Shubhammjangid <shubham.jangid@netsolutions.com>
2022-02-07 10:27:04 +0530

    Data Annotation

--- Models/StudentInfo.cs
+++ Models/StudentInfo.cs
@@ -7,12 +7,20 @@ namespace CRUD.Models
         
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Need to Enter your full Name")]
+        [Display(Name = "Full Name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Enter your College Name ")]
+        [Display(Name = "College Name")]
         public string CollegeName { get; set; }
 
+        [Required(ErrorMessage = "Enter your Course Name ")]
+        [Display(Name = "Course Name")]
         public string CourseName { get; set; }
 
+        [Required(ErrorMessage = "Enter your Address ")]
+        [Display(Name = "Address")]
         public string Address { get; set; }
     }
 }
